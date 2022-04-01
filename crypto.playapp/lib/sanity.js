0241d37d87fec4c5e40bc93b9ab1f428fda098a6
mr-dash30 <dash@Darshans-MacBook-Air.local>
2022-04-01 20:04:38 +0530

    first

---
+++ crypto.playapp/lib/sanity.js
@@ -0,0 +1,21 @@
+import sanityClient from '@sanity/client' 
+// jyuy6t2h
+
+const token = "skKVNGrCuHxNHAiWIXc6SrUWkT4sBWtYwBtzJ7rN0Y5YQKAA6pVVy7T2vMHVg1tpJ1sksrErUXY36qgDhg8FFzKvI8zg6qJM5m4cVdSaAmNosqWdmWHgzO9oBphAsIv0HnhPRJvIRdAuOnuW0iEL9ax4w2xbeZTGp8smDBLyV6gDL4rJ9RsI"
+
+export const client = sanityClient({
+    projectId: 'jyuy6t2h',
+    dataset: 'production',
+    apiVersion : 'v1',
+    token: token,
+    useCdn: false
+})
+
+// awaj kapay che 😭
+// wah
+// zip file kri? na
+// k kru
+// hu pelu web varu kru : metamask . k
+//oy
+// wow woooo hooo hihihihihi
+// convince afraid cup senior vicious orchard lonely end crater bind time vocal
---
+++ crypto.playapp/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+// npm install -g @sanity/cli && sanity init
+// thai gyu ne>
+// module folder kya che?
+//kyu
+//voice kem nai avto
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
+
+
+
